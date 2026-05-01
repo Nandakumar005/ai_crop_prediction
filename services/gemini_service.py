8e0fc77cd34f4255d0c6555d32b6c2b35afc926f
Nandakumar005 <nandakumardg8@gmail.com>
2026-05-01 16:08:47 +0530

    changes the gemini model for cheper rate

--- services/gemini_service.py
+++ services/gemini_service.py
@@ -6,7 +6,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
-GEMINI_MODEL = "gemini-3-flash-preview"
+GEMINI_MODEL = "gemini-2.0-flash"  
 
 def estimate_crop_values(weather, location, crops):
     url = f"https://generativelanguage.googleapis.com/v1beta/models/{GEMINI_MODEL}:generateContent"
